22fc2edda1080f1ac86010cb91e42bf090dd1229
omerkorkmaz01 <o.krkmz2004@gmail.com>
2023-02-28 10:10:53 +0300

    1.gun

---
+++ src/test/java/test/C3_JsonObjesiOlusturma.java
@@ -0,0 +1,65 @@
+package test;
+
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class C3_JsonObjesiOlusturma {
+
+    /*
+    Asagidaki JSON Objesini olusturup konsolda yazdirin.
+
+	{
+	    "title":"Ahmet",
+	    "body":"Merhaba",
+	    "userId":1
+	}
+     */
+    @Test
+    public void jasonObje01(){
+
+        JSONObject ilkJsonObje= new JSONObject();
+
+        ilkJsonObje.put("title","Ahmet");
+        ilkJsonObje.put("body","Merhaba");
+        ilkJsonObje.put("userId","1");
+
+        System.out.println(ilkJsonObje);
+    }
+
+    @Test
+    public void jsonObje02(){
+
+        /*
+        Asagidaki JSON Objesini olusturup konsolda yazdirin.
+    {
+     "firstname":"Jim",
+     "additionalneeds":"Breakfast",
+     "bookingdates":{
+             "checkin":"2018-01-01",
+             "checkout":"2019-01-01"
+	    },
+      "totalprice":111,
+      "depositpaid":true,
+      "lastname":"Brown"
+    }
+         */
+
+        JSONObject innerJsonObje= new JSONObject();
+
+        innerJsonObje.put("checkin","2018-01-01");
+        innerJsonObje.put("checkout","2019-01-01");
+
+        JSONObject body= new JSONObject();
+
+        body.put("firstname","Jim");
+        body.put("additionalneeds","Breakfast");
+        body.put("bookingdates",innerJsonObje);
+        body.put("totalprice",111);
+        body.put("depositpaid",true);
+        body.put("lastname","Brown");
+
+        System.out.println(body);
+    }
+
+}
