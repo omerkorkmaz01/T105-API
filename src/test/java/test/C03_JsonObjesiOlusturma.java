c02c7d8371ab524fca8b469874539d68240ed50a
omerkorkmaz01 <o.krkmz2004@gmail.com>
2023-02-28 10:13:57 +0300

    1.gun

--- src/test/java/test/C03_JsonObjesiOlusturma.java
+++ src/test/java/test/C03_JsonObjesiOlusturma.java
@@ -1,10 +1,9 @@
 package test;
 
-import netscape.javascript.JSObject;
 import org.json.JSONObject;
 import org.junit.Test;
 
-public class C3_JsonObjesiOlusturma {
+public class C03_JsonObjesiOlusturma {
 
     /*
     Asagidaki JSON Objesini olusturup konsolda yazdirin.
