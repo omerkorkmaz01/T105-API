062495c10476d9b6d904d75115e8a79ac6078c96
omerkorkmaz01 <o.krkmz2004@gmail.com>
2023-03-01 14:56:54 +0300

    3.gun

---
+++ src/test/java/test/C18_Get_TestDataClassKullanimi.java
@@ -0,0 +1,17 @@
+package test;
+
+public class C18_Get_TestDataClassKullanimi {
+    /*
+    https://jsonplaceholder.typicode.com/posts/22 url'ine bir GET request yolladigimizda donen response’in status kodunun 200 ve response body’sinin asagida verilen ile ayni oldugunutest ediniz
+
+    Response body :
+    {
+"userId":3,
+"id":22,
+"title":"dolor sint quo a velit explicabo quia nam",
+"body":"eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita ear
+        um mollitia molestiae aut atque rem suscipit\nnam impedit esse"
+    }
+    */
+
+}
